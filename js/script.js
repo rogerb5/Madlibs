194711578e724fa7a959cf55de3235dd05fbe4f8
rogerb5 <rbenjume@ewu.edu>
2023-02-09 18:55:55 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/*
+    Title: MadLibs
+    About: This program takes user input and generates
+    a silly short story.
+
+    Programmed by Roger 2/9/2023
+
+*/
+
+const storyText = `
+        There existed several 
+        :plural-noun: in the great Cascade Mountain Range. Each one of them had 
+        :adjective: hair. They always traveled to :place: once a year. 
+        On they way there they always said :silly-noise: in a :adjective: voice.
+        The group leader was named :persons-name:
+`;
+
+const pluralNounsArray = [];
+const adjectiveArray = [];
+const placeArray = [];
+const noiseArray = [];
+const nameArray = [];
+
+function processInput() {
+
+}
+
+function randomizeStory() {
+
+}
+
+function outputStory() {
+
+}
+
+function clearInput() {
+
+}
+
+function restartPrompts() {
+
+}

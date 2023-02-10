318ed32e2c2ebd2501f58190adebcab308f47292
rogerb5 <rbenjume@ewu.edu>
2023-02-09 20:24:47 -0800

    add sample story to text file and fix story string

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ const storyText = `
         There existed several 
         :plural-noun: in the great Cascade Mountain Range. Each one of them had 
         :adjective: hair. They always traveled to :place: once a year. 
-        On they way there they always said :silly-noise: in a :adjective: voice.
+        On the way there they always said :silly-noise: in a :adjective: voice.
         The group leader was named :persons-name:
 `;
 
